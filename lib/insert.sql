e4aba7e32b2209f79149c0c55294da196f5f3f1a
Charles <Charlessammarco@gmail.com>
2023-12-08 13:21:43 -0600

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 9, 'M', "Blue", "Hangry", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 9, 'F', "Blue", "Hangry", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 9, 'F', "Blue", "Hangry", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M', "Blue", "Hangry", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'F', "Blue", "Hangry", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', "Blue", "Hangry", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'F', "Blue", "Hangry", False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 9, 'M', "Blue", "Hangry", False);
